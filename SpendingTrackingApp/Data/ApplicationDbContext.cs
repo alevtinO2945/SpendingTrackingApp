beda3ed08f87d098708504b4058d6386d67b8a3d
Alevtinko <rakapaka2005@gmail.com>
2022-09-19 02:24:18 +0300

    Добавьте файлы проекта.

---
+++ SpendingTrackingApp/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SpendingTrackingApp.Models;
+
+namespace SpendingTrackingApp.Data
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options): base(options)
+        {
+
+        }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Transaction> Transactions { get; set; }
+    }
+}
